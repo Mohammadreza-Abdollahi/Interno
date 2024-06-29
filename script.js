efe600f1c2571ba5843cf40d3c50a106818307e9
Mohammadreza Abdollahi <mohammad1384abdollahi@gmail.com>
2024-06-29 22:54:43 +0330

    version 0.1.3
    added testimonial slider

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const workSwiper = new Swiper('.works-slider', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -9,8 +9,33 @@ const swiper = new Swiper('.swiper', {
   spaceBetween : 42,
   // Navigation arrows
   navigation: {
+    nextEl: '.works-swiper-button-next',
+    prevEl: '.works-swiper-button-prev',
+  },
+
+});
+const testimonialSwiper = new Swiper('.testimonial-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  autoplay: {
+    delay : 3000
+  },
+  // If we need pagination
+  pagination: {
+    el: '.testimonial-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
